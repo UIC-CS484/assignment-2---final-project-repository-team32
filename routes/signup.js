f10e245c1e1bd4079c1b830ac27bd4e7898413c5
Yusuf Khan <ykhan21@uic.edu>
2021-10-24 21:50:50 -0500

    export of passwordValidator

--- routes/signup.js
+++ routes/signup.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 const express = require('express')
 const router = express.Router()
 
+const passwordValidator = require('./validator.js')
+
 //create routes
 router.get('/',(req,res) => {
     res.render('signup.ejs') //name of the view index.ejs
@@ -37,34 +39,5 @@ router.post('/', (req,res) => {
         res.redirect('/login');
     }
 })
-
-let blacklistedPasswords = ["Password123","Passw0rd"];
-
-function passwordValidator(pswd) {
-  
-    let msg = "valid";
-    let isValid = false;
-    
-    if(/\s/.test(pswd)) {
-        msg = "password may not contain spaces"
-    } else if (!(/\d/.test(pswd) && /[a-zA-Z]/.test(pswd))) {
-        msg = "password must contain letter and a digit";
-    } else if (pswd.length < 8) {
-      msg = "password must have length greater than 8";
-    } else if (pswd.toLowerCase() === pswd) {
-        msg = "password needs uppercase letters";
-    } else if (pswd.toUpperCase() === pswd) {
-        msg = "password needs uppercase letters";
-    } else if(blacklistedPasswords.indexOf(pswd) > -1) {
-        msg = `password \"${pswd}\" blacklisted`
-    } else {
-        isValid=true;
-    }
-  
-    return {
-      "message": msg,
-      "isValid": isValid
-    }
-  }
 
 module.exports = router
---
+++ routes/validator.js
@@ -0,0 +1,30 @@
+let blacklistedPasswords = ["Password123","Passw0rd"];
+
+function passwordValidator(pswd) {
+  
+    let msg = "valid";
+    let isValid = false;
+    
+    if(/\s/.test(pswd)) {
+        msg = "password may not contain spaces"
+    } else if (!(/\d/.test(pswd) && /[a-zA-Z]/.test(pswd))) {
+        msg = "password must contain letter and a digit";
+    } else if (pswd.length < 8) {
+      msg = "password must have length greater than 8";
+    } else if (pswd.toLowerCase() === pswd) {
+        msg = "password needs uppercase letters";
+    } else if (pswd.toUpperCase() === pswd) {
+        msg = "password needs uppercase letters";
+    } else if(blacklistedPasswords.indexOf(pswd) > -1) {
+        msg = `password \"${pswd}\" blacklisted`
+    } else {
+        isValid=true;
+    }
+  
+    return {
+      "message": msg,
+      "isValid": isValid
+    }
+  }
+
+module.exports = passwordValidator
