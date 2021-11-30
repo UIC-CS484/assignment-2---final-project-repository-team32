15d294cd865820940bd54da36657314da3c8914f
Yusuf Khan <ykhan21@uic.edu>
2021-11-30 16:45:59 -0600

    added api test

--- passport-config.js
+++ passport-config.js
@@ -6,10 +6,10 @@ const bcrypt = require("bcrypt");
 function initialize(passport) {
   const authenticateUser = async (email, password, done) => {
     const user = await getUserByEmail(email);
-    console.log("got:", user, user.id);
     if (user == null) {
       return done(null, false, { message: "No user with that email" });
     }
+    console.log("got:", user, user.id);
 
     try {
       if (await bcrypt.compare(password, user.password)) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 //create routes
-router.get('/',(req,res) => {
-    res.render('index.ejs') //name of the view index.ejs
-})
+router.get("/", (req, res) => {
+  res.render("index.ejs", { name: req.user.name }); //name of the view index.ejs
+});
 
-module.exports = router
+module.exports = router;
