d88ba8b672bc53497ad6ecc138da8c5ece54d741
Yusuf Khan <ykhan21@uic.edu>
2021-12-01 02:16:44 -0600

    added api request on hompage with axios

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 //create routes
 router.get("/", (req, res) => {
   //   console.log(req);
-  res.render("index.ejs", { name: req.user?.name, data: "chicago" }); //name of the view index.ejs
+  getWeatherData().then((data) => {
+    res.render("index.ejs", { name: req.user?.name, data: "chicago " + data });
+  });
 });
 
-// router.delete("/logout", (req, res) => {
-//   req.logOut();
-//   res.redirect("/");
-// });
+function getWeatherData() {
+  let appId = process.env.APIKEY;
+  let units = "imperial"; // other option is metric
+  let searchMethod = "q";
+
+  let searchTerm = "chicago";
+
+  return axios
+    .get(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+    .then((result) => result.data.weather[0].description);
+}
 
 module.exports = router;
